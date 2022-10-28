091287dc30da9c14eac780112306076c897ce583
Casper <draakie@protonmail.com>
2022-10-28 20:42:18 +0200

    add login route

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_page():
+    return 'Hello, welcome to the home page!'
 
+@app.route('/login')
+def login():
+    return 'This is the login page!'
