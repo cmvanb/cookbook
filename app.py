a4a53d4927a7ca2907e63d4bbe3666de13f17968
Casper <draakie@protonmail.com>
2022-10-28 23:14:28 +0200

    Add pico.css, add templates for recipe view and recipes overview.

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home_page():
+def home():
     return 'Hello, welcome to the home page!'
 
 @app.route('/login')
 def login():
     return 'This is the login page!'
+
+@app.route('/recipe/')
+def recipes_overview():
+    return render_template('recipes-overview.html')
+
+@app.route('/recipe/<string:recipe_id>')
+def recipe_view(recipe_id):
+    return 'View recipe with id: %s' % recipe_id
