f48a1367cded783913d3f62cb15179a434bed12f
Casper <draakie@protonmail.com>
2022-10-29 18:04:43 +0200

    Messing around with Jinja templates.

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, welcome to the home page!'
+def index():
+    return redirect(url_for('recipes_overview'))
 
+# TODO: Implement authentication.
 @app.route('/login')
 def login():
     return 'This is the login page!'
 
-@app.route('/recipe/')
+@app.route('/recipe')
 def recipes_overview():
-    return render_template('recipes-overview.html')
+    return render_template('recipes-overview.html', seq=range(20))
 
 @app.route('/recipe/<string:recipe_id>')
 def recipe_view(recipe_id):
