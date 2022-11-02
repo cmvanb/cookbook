d7546685fb4cfdb9991f8350d63ccbb5af527c26
Casper <draakie@protonmail.com>
2022-11-02 10:20:41 +0100

    Fix quotation mistake in clean script.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,14 +4,14 @@ database=instance/cookbook.sqlite
 user_images_dir=cookbook/static/user_images
 
 if [[ -f "$database" ]]; then
-    rm "$database"
+    rm $database
     echo "Removed $database."
 else
     echo "$database doesn't exist."
 fi
 
 if [[ -d "$user_images_dir" ]]; then
-    rm "$user_images_dir/*"
+    rm $user_images_dir/*
     echo "Emptied $user_images_dir."
 else
     echo "$user_images_dir doesn't exist."
