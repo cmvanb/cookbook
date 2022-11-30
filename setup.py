7bd9e48aaeb2b0d917ebecac888ee7415595d6e1
Casper <draakie@protonmail.com>
2022-11-30 16:48:29 +0100

    Convert to setup.cfg, add test scaffolding.

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name='cookbook',
-    version='1.0.0',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
-)
+setup()
 
---
+++ tests/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO user (email, display_name, password)
+VALUES
+    ('test@example.com', 'Test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
+    ('other@example.com', 'Other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79');
+
+INSERT INTO recipe (user_id, created, title, author, description, source_url, image_path, servings, prep_time, cook_time, instructions)
+VALUES
+    (1, '2018-01-01 00:00:00', 'test recipe', 'chef ramsay', 'description', 'http://example.com', 'user_images/whatever', 2, 5, 10, 'instructions go here');
+
+INSERT INTO recipe_ingredient_map (recipe_id, input_text, count)
+VALUES
+    (1, '1tbsp nonsense', 1);
