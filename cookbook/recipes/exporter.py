27a9097d09b8caf8da75b75c98d0cf95e2e89e93
Casper <draakie@proton.me>
2024-06-15 18:14:20 +0200

    Minor cleanup.

--- cookbook/recipes/exporter.py
+++ cookbook/recipes/exporter.py
@@ -11,18 +11,18 @@ from cookbook.recipes import utils
 # Convert recipe model to YAML format.
 #-------------------------------------------------------------------------------
 def recipe_to_yaml(recipe, ingredients_list, instructions_list):
-    object = {}
-    object['title'] = recipe['title']
-    object['author'] = recipe['author']
-    object['description'] = recipe['description']
-    object['source_url'] = recipe['source_url']
-    object['servings'] = recipe['servings']
-    object['prep_time'] = recipe['prep_time']
-    object['cook_time'] = recipe['cook_time']
-    object['ingredients'] = ingredients_list
-    object['instructions'] = instructions_list
+    output = {}
+    output['title'] = recipe['title']
+    output['author'] = recipe['author']
+    output['description'] = recipe['description']
+    output['source_url'] = recipe['source_url']
+    output['servings'] = recipe['servings']
+    output['prep_time'] = recipe['prep_time']
+    output['cook_time'] = recipe['cook_time']
+    output['ingredients'] = ingredients_list
+    output['instructions'] = instructions_list
 
-    data = yaml.dump(object, width=math.inf, allow_unicode=True, sort_keys=False).encode()
+    data = yaml.dump(output, width=math.inf, allow_unicode=True, sort_keys=False).encode()
 
     file = io.BytesIO()
     file.write(data)
@@ -35,4 +35,3 @@ def recipe_to_yaml(recipe, ingredients_list, instructions_list):
 def recipe_file_name(title):
     title = utils.kebabcase(title)
     return f'{title}.yaml'
-
