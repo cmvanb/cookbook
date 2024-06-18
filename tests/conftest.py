02ef50083f85add72628acc1d28a8334e3ba44f4
Casper <draakie@proton.me>
2024-06-18 15:47:50 +0200

    Fix app import for tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 import pytest
 
-from cookbook import create_app
+from cookbook.application import create_app
 from cookbook.db import get_db, init_db
 
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,4 +1,4 @@
-from cookbook import create_app
+from cookbook.application import create_app
 
 def test_config():
     assert not create_app().testing
