a640febf5d0765e83fd4659b5a8e9ee5b3692b6e
Casper <draakie@proton.me>
2024-07-08 19:57:22 +0200

    Minor clean up.

--- cookbook/__init__.py
+++ cookbook/__init__.py
@@ -6,7 +6,3 @@ from cookbook.application import create_app
 
 # WSGI will hook onto this variable.
 app = create_app()
-
-# This is the entry point for the development server. Don't run on prod.
-if __name__ == '__main__':
-    app.run()
